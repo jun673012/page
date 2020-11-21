af93958a8a61b5f8041dc5d47f72da7121e5905a
sjh <sjh@DESKTOP-EH6HK3G>
2020-11-21 21:19:16 +0900

    html,css and servlet

---
+++ personalwebpage/src/aboutme/TodayServlet.java
@@ -0,0 +1,50 @@
+package aboutme;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/today")
+public class TodayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public TodayServlet() {
+        
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<a href='main.html'>메인화면</a>");
+		out.println("</body>");
+		out.println("</html>");
+		LocalDateTime currentDateTime = LocalDateTime.now();	
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+		String formatted = currentDateTime.format(formatter);
+		out.print("현재시간 : " + formatted);
+	}
+
+	
+	 
+	
+	/*
+	 * protected void doPost(HttpServletRequest request, HttpServletResponse
+	 * response) throws ServletException, IOException { // TODO Auto-generated
+	 * method stub doGet(request, response); }
+	 */
+
+}
